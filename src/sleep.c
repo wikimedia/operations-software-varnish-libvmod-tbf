0d499a8da2310909e04099db558b7823f4df3837
Sergey Poznyakoff <gray@gnu.org.ua>
2013-08-14 12:32:33 +0300

    New function tbf.sleep
    * src/Makefile.am: Add new file.
    * src/sleep.c: New file.
    * src/vmod-tbf.3: Document the sleep call.
    * src/vmod_tbf.vcc (sleep): New function.

---
+++ src/sleep.c
@@ -0,0 +1,33 @@
+/* This file is part of vmod-tbf
+   Copyright (C) 2013 Sergey Poznyakoff
+  
+   Vmod-tbf is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3, or (at your option)
+   any later version.
+  
+   Vmod-tbf is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+  
+   You should have received a copy of the GNU General Public License
+   along with vmod-tbf.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include <config.h>
+#include <errno.h>
+#include <time.h>
+#include "vrt.h"
+#include "vcc_if.h"
+
+void
+vmod_sleep(struct sess *sp, double t)
+{
+	struct timespec ts, ret;
+
+	ts.tv_sec = t;
+	ts.tv_nsec = (t - ts.tv_sec) * 1e9;
+
+	while (nanosleep(&ts, &ret) && errno == EINTR)
+		ts = ret;
+}
