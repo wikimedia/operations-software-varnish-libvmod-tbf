9c805521d05e9974e9c65de91d55c25f7ac5ae67
Sergey Poznyakoff <gray@gnu.org.ua>
2014-11-01 17:21:02 +0200

    Update copyright years

--- src/tbf.c
+++ src/tbf.c
@@ -1,5 +1,5 @@
 /* This file is part of vmod-tbf
-   Copyright (C) 2013 Sergey Poznyakoff
+   Copyright (C) 2013-2014 Sergey Poznyakoff
   
    Vmod-tbf is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
